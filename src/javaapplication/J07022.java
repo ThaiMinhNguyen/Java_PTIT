559db7293c1debf677c0efe0a6a6c9757a71bf93
MINH2201 <thaiminhnguyen2003@gmail.com>
2023-09-14 20:43:05 +0700

    Update

--- src/javaapplication/J07022.java
+++ src/javaapplication/J07022.java
@@ -8,8 +8,24 @@ package javaapplication;
  *
  * @author admin
  */
+import java.util.*;
+import java.io.*;
+
 public class J07022 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(new File("DATA.in"));
+        ArrayList<String> a = new ArrayList<>();
+        while(sc.hasNext()){
+            String s = sc.next();
+            try{
+                int t = Integer.parseInt(s);
+            } catch(Exception e){
+                a.add(s);
+            }
+            Collections.sort(a);
+        }
+        for(String i : a){
+            System.out.print(i + " ");
+        }
     }
 }
